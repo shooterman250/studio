0e90b2071d73c34669c397851bfd51a19d0c02e0
T <tdinteriorarchitecture@gmail.com>
2025-05-16 21:45:19 +0000

    Fix the logo as its not showing https://media.discordapp.net/attachments/1370568040256901200/1370582735122468954/butterfly_logo.png?ex=682897e4&is=68274664&hm=d1efad37b54995ce17b2917059ef5d7f3786ab33798c045302dc9cb1476519ca&=&format=webp&quality=lossless&width=1502&height=1502

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -22,7 +22,7 @@ const ButterflyLogo = ({ className, width, height }: ButterflyLogoProps) => {
       aria-label="Company Logo"
     >
       <Image
-        src="https://media.discordapp.net/attachments/1370568040256901200/1370582735122468954/butterfly_logo.png?ex=68200624&is=681eb4a4&hm=857aa242c852f51e2691ade9087a798c239d804caf79d4e04b0e1903c57337e9&=&format=webp&quality=lossless&width=1502&height=1502"
+        src="https://media.discordapp.net/attachments/1370568040256901200/1370582735122468954/butterfly_logo.png?ex=682897e4&is=68274664&hm=d1efad37b54995ce17b2917059ef5d7f3786ab33798c045302dc9cb1476519ca&=&format=webp&quality=lossless&width=1502&height=1502"
         alt="Interactive Designs Logo"
         layout="fill"
         objectFit="contain"
