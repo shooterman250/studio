8ed0ef7901efef7408f9faccac58dace1dd966bd
T <tdinteriorarchitecture@gmail.com>
2025-05-19 00:20:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_f31771ce._.js
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://media.discordapp.net/attachments/1370568040256901200/1373799883810672790/Biophilic_.png?ex=682bba58&is=682a68d8&hm=48c2fa6dcf5a914890087d71650f56908e9a60c78fa19a60741c7b52439f5291&=&format=webp&quality=lossless&width=774&height=774

--- src/app/(app)/overall-style/page.tsx
+++ src/app/(app)/overall-style/page.tsx
@@ -53,7 +53,7 @@ export default function OverallStylePage() {
     if (style.id === 'biophilic') {
       return {
         ...style,
-        imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1370575695373144224/Overall_Style_biophilic.png?ex=682b3455&is=6829e2d5&hm=d25337aa613c5b72296fbfd9070e35e2f7f5ab0d14f24869777d3f9d397f7dca&=&format=webp&quality=lossless&width=774&height=774' 
+        imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1373799883810672790/Biophilic_.png?ex=682bba58&is=682a68d8&hm=48c2fa6dcf5a914890087d71650f56908e9a60c78fa19a60741c7b52439f5291&=&format=webp&quality=lossless&width=774&height=774' 
       };
     }
     if (style.id === 'bohemian') {
@@ -128,7 +128,7 @@ export default function OverallStylePage() {
       title: "Select Key Elements", 
       description: "Choose guiding principles for your design.", 
       options: baseKeyElementOptions, // Key elements use their base options for display
-      cols: 3 // Changed from 4 to 3
+      cols: 3 
     }, 
   ];
 
