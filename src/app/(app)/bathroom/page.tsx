40dbea4c6e75ae218ad017ea23702a4c2bc01fa8
Kalie Howard <khinterdesigns@gmail.com>
2025-05-23 19:05:57 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_f31771ce._.js
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1746818708165.cluster-c3a7z3wnwzapkx3rfr5kz62dac.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    https://media.discordapp.net/attachments/1374799696127721638/1375493815799910500/Bathroom_Wall_Sconce.png?ex=6831e3f1&is=68309271&hm=fb02b3ecd5bd9f41636723eec526de40f20bb3e197fe1639996b6828c0ddcf9e&=&format=webp&quality=lossless&width=998&height=998

--- src/app/(app)/bathroom/page.tsx
+++ src/app/(app)/bathroom/page.tsx
@@ -27,6 +27,7 @@ const PAGE_STAGE_KEY: DesignStageKey = "bathroom";
 const newConsoleImageUrl = "https://media.discordapp.net/attachments/1374539386368167948/1374541051578024126/Console.png?ex=682e6c9c&is=682d1b1c&hm=9d427df1e7f3690029dd359522e2359d172c7fa8603e20f8cc5b32b1568523f5&=&format=webp&quality=lossless&width=1242&height=1242";
 const newChandelierImageUrl = "https://media.discordapp.net/attachments/1374799696127721638/1375493814978084996/Bathroom_Chandelier.png?ex=6831e3f0&is=68309270&hm=1448fa9f48ca6a3767e55ed20bf142dd54e4f11a90762e6a6bd716ea3bbe67cf&=&format=webp&quality=lossless&width=998&height=998";
 const newPendantImageUrl = "https://media.discordapp.net/attachments/1374799696127721638/1375493815409971261/Bathroom_Pendant.png?ex=6831e3f1&is=68309271&hm=cc0dad3a78229a8b3fde0be467e0350b1fb76d0e6e8b1c0e072b8e39381a4cdc&=&format=webp&quality=lossless&width=998&height=998";
+const newWallSconceImageUrl = "https://media.discordapp.net/attachments/1374799696127721638/1375493815799910500/Bathroom_Wall_Sconce.png?ex=6831e3f1&is=68309271&hm=fb02b3ecd5bd9f41636723eec526de40f20bb3e197fe1639996b6828c0ddcf9e&=&format=webp&quality=lossless&width=998&height=998";
 
 export default function BathroomPage() {
   const [selectedOptions, setSelectedOptions] = useState<Set<string>>(new Set());
@@ -157,6 +158,9 @@ export default function BathroomPage() {
       if (option.id === 'light-pendant') {
         imageUrl = newPendantImageUrl;
       }
+      if (option.id === 'light-wallsconce') {
+        imageUrl = newWallSconceImageUrl;
+      }
       return { ...option, name, imageUrl };
     });
 
@@ -171,6 +175,9 @@ export default function BathroomPage() {
       }
       if (option.id === 'light-pendant') {
         imageUrl = newPendantImageUrl;
+      }
+      if (option.id === 'light-wallsconce') {
+        imageUrl = newWallSconceImageUrl;
       }
       return { ...option, name, imageUrl };
     });
