042d5452a95b8fd8ca1c5ed2935e0b94181714a4
T <tdinteriorarchitecture@gmail.com>
2025-05-28 09:44:39 +0000

    when i press the start designing button this should take me to the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function LandingPage() {
         </p>
         {mounted ? (
           <Link
-            href="/overall-budget" 
+            href="/designer" 
             className={cn(
               buttonVariants({ variant: 'default', size: 'lg' }),
               'text-lg px-10 py-6 shadow-lg hover:shadow-xl transition-shadow duration-300'
