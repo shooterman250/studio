4c7a18b01b7b44cd98052b687114b935e89cc129
Kalie Howard <khinterdesigns@gmail.com>
2025-06-17 21:14:57 +0000

    the buttons that i have specified need to light up green because right n

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-card text-primary-foreground hover:bg-card/[.90]", // Changed from bg-foreground
+        default: "bg-card text-primary-foreground hover:bg-card/[.90]",
+        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
