44cdaa312e5b77db014584dde69c1b1658794bb7
T <tdinteriorarchitecture@gmail.com>
2025-06-22 23:51:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    wit

--- src/data/bedroomOptions.ts
+++ src/data/bedroomOptions.ts
@@ -21,14 +21,32 @@ export const bedroomLightingOptions: BaseSelectionItem[] = [
   },
   {
     id: 'bed-light-wall',
-    name: 'Wall Light(s)',
+    name: 'Wall Sconce(s)',
     imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1383581233262362814/Wall_Sconce_Lighting.png?ex=684f4fef&is=684dfe6f&hm=73ce983263514ea42edb988314d31c4186ed1b266ea19d36f222651f4b01b0e6&=&format=webp&quality=lossless&width=966&height=966',
     dataAiHint: 'wall sconce bedroom',
   },
   {
-    id: 'bed-light-ambient',
-    name: 'Ambient Soft Lighting',
+    id: 'bed-light-pendant',
+    name: 'Pendant Light(s)',
     imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1383583432038158490/Pendant_Lighting_.png?ex=684f51fb&is=684e007b&hm=d73c22534451ebecbb36d9e0f2d5ed6fa47e3eb8af88e0009e3c93d8953fc35c&=&format=webp&quality=lossless&width=1174&height=1174',
-    dataAiHint: 'ambient lighting bedroom',
+    dataAiHint: 'pendant light bedroom',
   },
-];
+  {
+    id: 'bed-light-picture',
+    name: 'Picture/Art Lighting',
+    imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1384235952045821963/Picture_Art_Lighting_.png?ex=6855a630&is=685454b0&hm=2459109d03204825b4f04693067613906980b9ee9d44e64647cb06ab8c317ec9&=&format=webp&quality=lossless&width=741&height=741',
+    dataAiHint: 'picture art lighting',
+  },
+  {
+    id: 'bed-light-flush-mount',
+    name: 'Flush Mount Lighting',
+    imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1384235950720155679/Flush_Mount_Lighting.png?ex=6855a630&is=685454b0&hm=67641c58d761d8e36c271be21ba5cc20131da308a6f77fe384b18186e6919f24&=&format=webp&quality=lossless&width=741&height=741',
+    dataAiHint: 'flush mount lighting',
+  },
+  {
+    id: 'bed-light-ceiling-fan',
+    name: 'Ceiling Fan With Light',
+    imageUrl: 'https://media.discordapp.net/attachments/1370568040256901200/1384235948887507104/Ceiling_Fan_With_Light_.png?ex=6855a630&is=685454b0&hm=76183e211473c4afc93142e7a4ddfeb738facfde67e776a4d3bfd97ab918181f&=&format=webp&quality=lossless&width=741&height=741',
+    dataAiHint: 'ceiling fan light',
+  }
+].sort((a, b) => a.name.localeCompare(b.name));
